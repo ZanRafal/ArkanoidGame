b778251b6e0b6f5405878c191bc810047a746738
ZanRafal <01159225@pw.edu.pl>
2021-03-25 10:02:02 +0100

    Added Ball and Paddle references, created List of Brick objects, added getters and setters

--- src/Arkanoid.java
+++ src/Arkanoid.java
@@ -1,12 +1,16 @@
+import java.util.List;
+
 public class Arkanoid {
+    private int height;
+    private int width;
+    private Paddle paddle;
+    private Ball ball;
+    private List<Brick> bricks;
 
     public Arkanoid(int height, int width) {
         this.height = height;
         this.width = width;
     }
-
-    private int height;
-    private int width;
 
     public int getHeight() {
         return height;
@@ -22,6 +26,30 @@ public class Arkanoid {
 
     public void setWidth(int width) {
         this.width = width;
+    }
+
+    public Paddle getPaddle() {
+        return paddle;
+    }
+
+    public void setPaddle(Paddle paddle) {
+        this.paddle = paddle;
+    }
+
+    public Ball getBall() {
+        return ball;
+    }
+
+    public void setBall(Ball ball) {
+        this.ball = ball;
+    }
+
+    public List<Brick> getBricks() {
+        return bricks;
+    }
+
+    public void setBricks(List<Brick> bricks) {
+        this.bricks = bricks;
     }
 
     public static void main(String[] args) {
